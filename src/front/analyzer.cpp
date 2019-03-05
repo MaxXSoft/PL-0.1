19b621754cb2ca51aa2802afd5b8c1b048819b40
MaxXSoft <x@MaxXSoft.net>
2019-03-05 20:22:41 +0800

    (analyzer) completed implementation of class 'Analyzer'

--- src/front/analyzer.cpp
+++ src/front/analyzer.cpp
@@ -4,8 +4,12 @@
 
 namespace {
 
-inline bool IsError(SymbolType type) {
-    return type == SymbolType::Error;
+inline bool IsError(SymbolInfo info) {
+    return info.type == SymbolType::Error;
+}
+
+inline bool IsConstOrVar(SymbolType type) {
+    return type == SymbolType::Const || type == SymbolType::Var;
 }
 
 } // namespace
@@ -32,62 +36,116 @@ SymbolType Analyzer::AnalyzeConst(const std::string &id, SymbolType init,
         return PrintError("initialize with non-constant value",
                 id.c_str(), line_pos);
     }
-    if (!IsError(env_->GetType(id))) {
+    if (!IsError(env_->GetInfo(id))) {
         return PrintError("identifier has already been defined",
                 id.c_str(), line_pos);
     }
-    env_->AddSymbol(id, SymbolType::Const);
+    env_->AddSymbol(id, {SymbolType::Const, 0});
     return SymbolType::Void;
 }
 
 SymbolType Analyzer::AnalyzeVar(const std::string &id, SymbolType init,
         unsigned int line_pos) {
-    if (init != SymbolType::Const && init != SymbolType::Var) {
+    if (!IsConstOrVar(init)) {
         return PrintError("invalid initial value", id.c_str(), line_pos);
     }
-    if (!IsError(env_->GetType(id))) {
+    if (!IsError(env_->GetInfo(id))) {
         return PrintError("identifier has already been defined",
                 id.c_str(), line_pos);
     }
-    env_->AddSymbol(id, SymbolType::Var);
+    env_->AddSymbol(id, {SymbolType::Var, 0});
     return SymbolType::Void;
 }
 
 SymbolType Analyzer::AnalyzeProcedure(const std::string &id,
         unsigned int line_pos) {
-    //
+    if (!IsError(env_->GetInfo(id))) {
+        return PrintError("identifier has already been defined",
+                id.c_str(), line_pos);
+    }
+    env_->AddSymbol(id, {SymbolType::Proc, 0});
+    return SymbolType::Void;
 }
 
 SymbolType Analyzer::AnalyzeFunction(const std::string &id,
         const IdList &args, unsigned int line_pos) {
-    //
+    if (!IsError(env_->GetInfo(id))) {
+        return PrintError("identifier has already been defined",
+                id.c_str(), line_pos);
+    }
+    // add argument id to current environment
+    for (const auto &i : args) env_->AddSymbol(i, {SymbolType::Var, 0});
+    // add function id to outer environment
+    env_->outer()->AddSymbol(id, {SymbolType::Func, args.size()});
+    return SymbolType::Void;
 }
 
 SymbolType Analyzer::AnalyzeAssign(const std::string &id,
         SymbolType expr_type, unsigned int line_pos) {
-    //
+    auto info = env_->GetInfo(id);
+    if (IsError(info)) {
+        return PrintError("identifier has not been defined",
+                id.c_str(), line_pos);
+    }
+    if (info.type != SymbolType::Var) {
+        return PrintError("try to assign a value to a non-variable",
+                id.c_str(), line_pos);
+    }
+    if (!IsConstOrVar(expr_type)) {
+        return PrintError("invalid assignment", id.c_str(), line_pos);
+    }
+    return SymbolType::Void;
 }
 
 SymbolType Analyzer::AnalyzeControl(unsigned int line_pos) {
-    //
+    if (!while_count_) {
+        return PrintError("try to use break/continue outside 'while' loop",
+                line_pos);
+    }
+    return SymbolType::Void;
 }
 
 SymbolType Analyzer::AnalyzeUnary(SymbolType operand,
         unsigned int line_pos) {
-    //
+    if (!IsConstOrVar(operand)) {
+        return PrintError("invalid operand", line_pos);
+    }
+    return operand;
 }
 
 SymbolType Analyzer::AnalyzeBinary(SymbolType lhs, SymbolType rhs,
         unsigned int line_pos) {
-    //
+    if (!IsConstOrVar(lhs)) return PrintError("invalid lhs", line_pos);
+    if (!IsConstOrVar(rhs)) return PrintError("invalid rhs", line_pos);
+    if (lhs == SymbolType::Const && rhs == SymbolType::Const) {
+        return SymbolType::Const;
+    }
+    return SymbolType::Var;
 }
 
 SymbolType Analyzer::AnalyzeFunCall(const std::string &id, int arg_count,
         unsigned int line_pos) {
-    //
+    auto info = env_->GetInfo(id);
+    if (info.type != SymbolType::Func) {
+        return PrintError("try to call a non-function",
+                id.c_str(), line_pos);
+    }
+    if (info.func_arg_count != arg_count) {
+        return PrintError("argument count mismatch", id.c_str(), line_pos);
+    }
+    return SymbolType::Var;
 }
 
 SymbolType Analyzer::AnalyzeId(const std::string &id,
         unsigned int line_pos) {
-    //
+    auto info = env_->GetInfo(id);
+    if (IsError(info)) {
+        return PrintError("identifier has not been defined",
+                id.c_str(), line_pos);
+    }
+    switch (info.type) {
+        case SymbolType::Proc: return SymbolType::Void;
+        case SymbolType::Func: return AnalyzeFunCall(id, 0, line_pos);
+        default: return info.type;
+    }
 }
