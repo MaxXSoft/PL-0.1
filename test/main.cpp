9c9b52b2de9bbe8342e1acdf3f4cc8d20cd775c8
MaxXSoft <x@MaxXSoft.net>
2019-03-05 09:10:24 +0800

    (test/main) added main file

---
+++ test/main.cpp
@@ -0,0 +1,14 @@
+#include <test.h>
+
+#define ALL_TESTS(f) f(LexerTest)
+
+// expand function declarations & unit test array
+UNIT_TEST(ALL_TESTS, unit_test);
+
+int main(int argc, const char *argv[]) {
+    // run all unit tests
+    for (const auto &i : unit_test) i();
+    // display result & return
+    Test::ShowResult();
+    return Test::is_passed() ? 0 : 1;
+}
