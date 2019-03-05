dfbfdd5285376f26a649cb455038c02f10918579
MaxXSoft <x@MaxXSoft.net>
2019-03-05 19:38:28 +0800

    (analyzer) completed interface definition of class 'Analyzer'

--- src/front/analyzer.cpp
+++ src/front/analyzer.cpp
@@ -1,9 +1,93 @@
 #include <front/analyzer.h>
 
+#include <iostream>
+
 namespace {
 
-//
+inline bool IsError(SymbolType type) {
+    return type == SymbolType::Error;
+}
 
 } // namespace
 
-//
+SymbolType Analyzer::PrintError(const char *message,
+        unsigned int line_pos) {
+    std::cerr << "analyzer (line " << line_pos;
+    std::cerr << "): error: " << message << std::endl;
+    ++error_num_;
+    return SymbolType::Error;
+}
+
+SymbolType Analyzer::PrintError(const char *message, const char *id,
+        unsigned int line_pos) {
+    std::cerr << "analyzer (line " << line_pos;
+    std::cerr << ", id: " << id << "): error: " << message << std::endl;
+    ++error_num_;
+    return SymbolType::Error;
+}
+
+SymbolType Analyzer::AnalyzeConst(const std::string &id, SymbolType init,
+        unsigned int line_pos) {
+    if (init != SymbolType::Const) {
+        return PrintError("initialize with non-constant value",
+                id.c_str(), line_pos);
+    }
+    if (!IsError(env_->GetType(id))) {
+        return PrintError("identifier has already been defined",
+                id.c_str(), line_pos);
+    }
+    env_->AddSymbol(id, SymbolType::Const);
+    return SymbolType::Void;
+}
+
+SymbolType Analyzer::AnalyzeVar(const std::string &id, SymbolType init,
+        unsigned int line_pos) {
+    if (init != SymbolType::Const && init != SymbolType::Var) {
+        return PrintError("invalid initial value", id.c_str(), line_pos);
+    }
+    if (!IsError(env_->GetType(id))) {
+        return PrintError("identifier has already been defined",
+                id.c_str(), line_pos);
+    }
+    env_->AddSymbol(id, SymbolType::Var);
+    return SymbolType::Void;
+}
+
+SymbolType Analyzer::AnalyzeProcedure(const std::string &id,
+        unsigned int line_pos) {
+    //
+}
+
+SymbolType Analyzer::AnalyzeFunction(const std::string &id,
+        const IdList &args, unsigned int line_pos) {
+    //
+}
+
+SymbolType Analyzer::AnalyzeAssign(const std::string &id,
+        SymbolType expr_type, unsigned int line_pos) {
+    //
+}
+
+SymbolType Analyzer::AnalyzeControl(unsigned int line_pos) {
+    //
+}
+
+SymbolType Analyzer::AnalyzeUnary(SymbolType operand,
+        unsigned int line_pos) {
+    //
+}
+
+SymbolType Analyzer::AnalyzeBinary(SymbolType lhs, SymbolType rhs,
+        unsigned int line_pos) {
+    //
+}
+
+SymbolType Analyzer::AnalyzeFunCall(const std::string &id, int arg_count,
+        unsigned int line_pos) {
+    //
+}
+
+SymbolType Analyzer::AnalyzeId(const std::string &id,
+        unsigned int line_pos) {
+    //
+}
--- src/inc/front/analyzer.h
+++ src/inc/front/analyzer.h
@@ -3,16 +3,50 @@
 
 #include <string>
 
+#include <define/type.h>
 #include <define/symbol.h>
 
 class Analyzer {
 public:
-    Analyzer() {}
+    Analyzer() : env_(std::make_shared<Environment>()),
+            error_num_(0), while_count_(0) {}
+    Analyzer(const EnvPtr &env)
+            : env_(env), error_num_(0), while_count_(0) {}
 
-    SymbolType AnalyzeConst(const std::string &id, unsigned int line_num);
+    SymbolType AnalyzeConst(const std::string &id, SymbolType init,
+            unsigned int line_pos);
+    SymbolType AnalyzeVar(const std::string &id, SymbolType init,
+            unsigned int line_pos);
+    SymbolType AnalyzeProcedure(const std::string &id,
+            unsigned int line_pos);
+    SymbolType AnalyzeFunction(const std::string &id, const IdList &args,
+            unsigned int line_pos);
+    SymbolType AnalyzeAssign(const std::string &id, SymbolType expr_type,
+            unsigned int line_pos);
+    SymbolType AnalyzeControl(unsigned int line_pos);
+    SymbolType AnalyzeUnary(SymbolType operand, unsigned int line_pos);
+    SymbolType AnalyzeBinary(SymbolType lhs, SymbolType rhs,
+            unsigned int line_pos);
+    SymbolType AnalyzeFunCall(const std::string &id, int arg_count,
+            unsigned int line_pos);
+    SymbolType AnalyzeId(const std::string &id, unsigned int line_pos);
+
+    void NewEnvironment() { env_ = std::make_shared<Environment>(env_); }
+    void RestoreEnvironment() { env_ = env_->outer(); }
+    void EnterWhile() { ++while_count_; }
+    void ExitWhile() { --while_count_; }
+
+    unsigned int error_num() const { return error_num_; }
+    const EnvPtr &env() const { return env_; }
 
 private:
+    SymbolType PrintError(const char *message, unsigned int line_pos);
+    SymbolType PrintError(const char *message, const char *id,
+            unsigned int line_pos);
+
+    unsigned int error_num_;
     EnvPtr env_;
+    int while_count_;
 };
 
 #endif // PL01_FRONT_ANALYZER_H_
