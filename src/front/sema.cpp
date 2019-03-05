1ed73111fe83e70fe4c54e74fc9dda208b02a47f
MaxXSoft <x@MaxXSoft.net>
2019-03-05 16:42:38 +0800

    (sema) added file 'sema.cpp'

---
+++ src/front/sema.cpp
@@ -0,0 +1,65 @@
+#include <define/ast.h>
+
+SymbolType BlockAST::SemaAnalyze(Analyzer &ana) {
+    //
+}
+
+SymbolType ConstsAST::SemaAnalyze(Analyzer &ana) {
+    //
+}
+
+SymbolType VarsAST::SemaAnalyze(Analyzer &ana) {
+    //
+}
+
+SymbolType ProcedureAST::SemaAnalyze(Analyzer &ana) {
+    //
+}
+
+SymbolType FunctionAST::SemaAnalyze(Analyzer &ana) {
+    //
+}
+
+SymbolType AssignAST::SemaAnalyze(Analyzer &ana) {
+    //
+}
+
+SymbolType BeginEndAST::SemaAnalyze(Analyzer &ana) {
+    //
+}
+
+SymbolType IfAST::SemaAnalyze(Analyzer &ana) {
+    //
+}
+
+SymbolType WhileAST::SemaAnalyze(Analyzer &ana) {
+    //
+}
+
+SymbolType AsmAST::SemaAnalyze(Analyzer &ana) {
+    //
+}
+
+SymbolType ControlAST::SemaAnalyze(Analyzer &ana) {
+    //
+}
+
+SymbolType UnaryAST::SemaAnalyze(Analyzer &ana) {
+    //
+}
+
+SymbolType BinaryAST::SemaAnalyze(Analyzer &ana) {
+    //
+}
+
+SymbolType FunCallAST::SemaAnalyze(Analyzer &ana) {
+    //
+}
+
+SymbolType IdAST::SemaAnalyze(Analyzer &ana) {
+    //
+}
+
+SymbolType NumberAST::SemaAnalyze(Analyzer &ana) {
+    //
+}
