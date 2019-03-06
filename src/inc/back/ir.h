7d2856e1d9ff85a2d140205445aa322230a7b18b
MaxXSoft <x@MaxXSoft.net>
2019-03-06 19:30:16 +0800

    (ir) modified the interface of IR

--- src/inc/back/ir.h
+++ src/inc/back/ir.h
@@ -2,12 +2,24 @@
 #define PL01_DEFINE_IR_H_
 
 #include <memory>
+#include <any>
+#include <cassert>
 
 class IRBase {
 public:
     virtual ~IRBase() = default;
+
+    virtual const std::any value() const = 0;
 };
 
 using IRPtr = std::shared_ptr<IRBase>;
+
+template <typename T>
+const T IRCast(const IRPtr &ir) {
+    auto ret = ir->value();
+    auto value = std::any_cast<T>(&ret);
+    assert(value != nullptr);
+    return *value;
+}
 
 #endif // PL01_DEFINE_IR_H_
