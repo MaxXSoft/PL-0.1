7170ac8bdcac271c705df6ba0d5a157bc34951bb
MaxXSoft <x@MaxXSoft.net>
2019-03-06 10:56:52 +0800

    (type) added a new type

--- src/inc/define/type.h
+++ src/inc/define/type.h
@@ -4,8 +4,22 @@
 #include <vector>
 #include <string>
 
+/*
+
+type of symbol:
+    Error:  sematic analysis error
+    Const:  integer constant
+    Var:    integer variable
+    Proc:   procedure
+    Func:   function that returns an integer
+    Ret:    reference to a function,
+            represents both the function return value and function itself
+    Void:   void type (type of general statements)
+
+*/
+
 enum class SymbolType {
-    Error, Const, Var, Proc, Func, Void
+    Error, Const, Var, Proc, Func, Ret, Void
 };
 
 using IdList = std::vector<std::string>;
