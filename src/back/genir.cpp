ca2d1c9b069d0da6863bb3f4c149ededb0a5b53e
MaxXSoft <x@MaxXSoft.net>
2019-03-06 18:36:02 +0800

    (genir) added file 'genir.cpp'

---
+++ src/back/genir.cpp
@@ -0,0 +1,61 @@
+#include <define/ast.h>
+
+IRPtr BlockAST::GenerateIR(IRBuilder &irb) {
+    //
+}
+
+IRPtr ConstsAST::GenerateIR(IRBuilder &irb) {
+    //
+}
+
+IRPtr VarsAST::GenerateIR(IRBuilder &irb) {
+    //
+}
+
+IRPtr ProcedureAST::GenerateIR(IRBuilder &irb) {
+    //
+}
+
+IRPtr FunctionAST::GenerateIR(IRBuilder &irb) {
+    //
+}
+
+IRPtr AssignAST::GenerateIR(IRBuilder &irb) {
+    //
+}
+
+IRPtr IfAST::GenerateIR(IRBuilder &irb) {
+    //
+}
+
+IRPtr WhileAST::GenerateIR(IRBuilder &irb) {
+    //
+}
+
+IRPtr AsmAST::GenerateIR(IRBuilder &irb) {
+    //
+}
+
+IRPtr ControlAST::GenerateIR(IRBuilder &irb) {
+    //
+}
+
+IRPtr UnaryAST::GenerateIR(IRBuilder &irb) {
+    //
+}
+
+IRPtr BinaryAST::GenerateIR(IRBuilder &irb) {
+    //
+}
+
+IRPtr FunCallAST::GenerateIR(IRBuilder &irb) {
+    //
+}
+
+IRPtr IdAST::GenerateIR(IRBuilder &irb) {
+    //
+}
+
+IRPtr NumberAST::GenerateIR(IRBuilder &irb) {
+    //
+}
