799bf1d98c9c913366af751b7527b4612c6ee151
MaxXSoft <x@MaxXSoft.net>
2019-03-07 16:50:09 +0800

    (irbuilder) added some interface definitions

--- src/inc/back/irbuilder.h
+++ src/inc/back/irbuilder.h
@@ -1,13 +1,38 @@
 #ifndef PL01_BACK_IRBUILDER_H_
 #define PL01_BACK_IRBUILDER_H_
 
+#include <string>
+
+#include <front/lexer.h>
 #include <back/ir.h>
+#include <define/type.h>
 
 class IRBuilder {
 public:
     virtual ~IRBuilder() = default;
 
-    virtual IRPtr GenerateBlock() = 0;
+    virtual IRPtr GenerateBlock(LazyIRGen consts, LazyIRGen vars,
+            LazyIRGen proc_func, LazyIRGen stat) = 0;
+    virtual IRPtr GenerateConst(const std::string &id,
+            const IRPtr &expr) = 0;
+    virtual IRPtr GenerateVar(const std::string &id, const IRPtr &init) = 0;
+    virtual IRPtr GenerateProcedure(const std::string &id,
+            LazyIRGen block) = 0;
+    virtual IRPtr GenerateFunction(const std::string &id,
+            const IdList &args, LazyIRGen block) = 0;
+    virtual IRPtr GenerateAssign(const std::string &id,
+            const IRPtr &expr) = 0;
+    virtual IRPtr GenerateIf(const IRPtr &cond, LazyIRGen then,
+            LazyIRGen else_then) = 0;
+    virtual IRPtr GenerateWhile(const IRPtr &cond, LazyIRGen body) = 0;
+    virtual IRPtr GenerateAsm(const std::string &asm_str) = 0;
+    virtual IRPtr GenerateControl(Lexer::Keyword type) = 0;
+    virtual IRPtr GenerateUnary(const IRPtr &operand) = 0;  // 'odd' only
+    virtual IRPtr GenerateBinary(Lexer::Operator op,
+            const IRPtr &lhs, const IRPtr &rhs) = 0;
+    virtual IRPtr GenerateFunCall(const std::string &id,
+            const IRPtrList &args) = 0;
+    virtual IRPtr GenerateId(const std::string &id) = 0;
     virtual IRPtr GenerateNumber(int value) = 0;
 };
 
