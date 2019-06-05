d0f7ec65b235fe00cab9fd61aefabfa79259622e
MaxXSoft <x@MaxXSoft.net>
2019-06-05 16:25:37 +0800

    (parser) updated unit test

--- test/unit/parser_test.cpp
+++ test/unit/parser_test.cpp
@@ -8,56 +8,58 @@ using namespace std;
 
 namespace {
 
-const char *program0 =
-        "const a = 1, b = 2;\n"
-        "var c = 3, d = a * 2 + 1;\n"
-        "\n"
-        "procedure test;\n"
-        "begin\n"
-        "   c := -d * 6 + 10;\n"
-        "   write(c)\n"
-        "end;\n"
-        "\n"
-        "function test2(a);\n"
-        "begin\n"
-        "   if a <= 10 then begin\n"
-        "       test2 := 1;\n"
-        "   end\n"
-        "   else if a > 100 then begin\n"
-        "       test2 := 2;\n"
-        "   end;\n"
-        "end;\n"
-        "\n"
-        "begin\n"
-        "   test;\n"
-        "   test2(d + 3, e);\n"
-        "end.";
+const char *program0 = R"raw(
+    const a = 1, b = 2;
+    var c = 3, d = a * 2 + 1;
 
-const char *program1 =
-        "function fib(n);\n"
-        "begin\n"
-        "   if n < 1 then begin\n"
-        "       fib := -1;\n"
-        "   end\n"
-        "   else if n <= 2 then begin\n"
-        "       fib := 1;\n"
-        "   end\n"
-        "   else begin\n"
-        "       fib := fib(n - 1) + fib(n - 2);\n"
-        "   end;\n"
-        "end;\n"
-        "\n"
-        "procedure main;\n"
-        "var in;\n"
-        "begin\n"
-        "   in := 1;\n"
-        "   while in >= 1 do begin\n"
-        "       read(in);\n"
-        "       write(fib(in));\n"
-        "   end;\n"
-        "end;\n"
-        "\n"
-        "main.";
+    procedure test;
+    begin
+        c := -d * 6 + 10;
+        write(c)
+    end;
+
+    function test2(a);
+    begin
+        if a <= 10 then begin
+            test2 := 1;
+        end
+        else if a > 100 then begin
+            test2 := 2;
+        end;
+    end;
+
+    begin
+        test;
+        test2(d + 3, e);
+    end.
+)raw";
+
+const char *program1 = R"raw(
+    function fib(n);
+    begin
+    if n < 1 then begin
+        fib := -1;
+    end
+    else if n <= 2 then begin
+        fib := 1;
+    end
+    else begin
+        fib := fib(n - 1) + fib(n - 2);
+    end;
+    end;
+
+    procedure main;
+    var in;
+    begin
+    in := 1;
+    while in >= 1 do begin
+        read(in);
+        write(fib(in));
+    end;
+    end;
+
+    main.
+)raw";
 
 } // namespace
 
